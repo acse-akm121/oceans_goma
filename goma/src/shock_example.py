dee7613659edaa214ae6dd35f2c307c78835c5fc
akhil <akm121@ic.ac.uk>
2022-08-10 17:35:30 +0100

    Update .py file to reflect progress

--- goma/src/shock_example.py
+++ goma/src/shock_example.py
@@ -5,7 +5,9 @@ from firedrake import *
 from pyroteus import *
 from pyroteus_adjoint import *
 import matplotlib.pyplot as plt
+from movement import *
 
+plt.rcParams["figure.facecolor"] = "white"  # Ensure  background on plots is white
 
 NU = 0.0001
 
@@ -73,11 +75,11 @@ def get_qoi(mesh_seq, solutions, i):
     return end_time_qoi
 
 
-def plot_indicator_snapshots2(indicators, time_partition, **kwargs):
+def plot_indicators(indicators, time_partition, **kwargs):
     """
     Plot a sequence of snapshots associated with
     ``indicators`` and :class:`TimePartition`
-    ``time_partition``. This modification plots the log of errors.
+    ``time_partition``.
 
     Any keyword arguments are passed to ``tricontourf``.
 
@@ -87,24 +89,25 @@ def plot_indicator_snapshots2(indicators, time_partition, **kwargs):
         object used to solve the problem
     """
     P = time_partition
-    rows = P.exports_per_subinterval[0] - 1
-    cols = P.num_subintervals
+    rows = kwargs.pop("rows", (P.exports_per_subinterval[0] - 1) // 2)
+    cols = kwargs.pop("cols", (P.num_subintervals * 2))
     steady = rows == cols == 1
+    print(rows, cols)
     figsize = kwargs.pop("figsize", (6 * cols, 24 // cols))
     fig, axes = plt.subplots(rows, cols, sharex="col", figsize=figsize)
     tcs = []
-    for i, indi_step in enumerate(indicators):
-        ax = axes if steady else axes[0] if cols == 1 else axes[0, i]
-        ax.set_title(f"Mesh[{i}]")
+    idx = -1
+    for i in range(rows):
         tc = []
-        for j, indi in enumerate(indi_step):
-            t_indi = Function(indi.function_space(), val=np.log(indi.dat.data))
-            ax = axes if steady else axes[j] if cols == 1 else axes[j, i]
-            tc.append(tricontourf(t_indi, axes=ax, **kwargs))
+        for j in range(cols):
+            idx += 1
+            ax = axes[i, j]
+            # Element-wise norm for the indicators
+            tc.append(tricontourf(indicators[0][idx], axes=ax, **kwargs))
             if not steady:
                 time = (
                     i * P.end_time / cols
-                    + j * P.timesteps_per_export[i] * P.timesteps[i]
+                    + j * P.timesteps_per_export[0] * P.timesteps[0]
                 )
                 ax.annotate(f"t={time:.2f}", (0.05, 0.05), color="white")
         tcs.append(tc)
@@ -112,14 +115,82 @@ def plot_indicator_snapshots2(indicators, time_partition, **kwargs):
     return fig, axes, tcs
 
 
+def plot_mesh(mesh, fig=None, axes=None, time_partition=None, **kwargs):
+    """
+    Plot the given mesh as a surface with edges.
+    """
+    kwargs.setdefault("interior_kw", {"linewidth": 0.5})
+    kwargs.setdefault("boundary_kw", {"linewidth": 2.0})
+    if fig is None and axes is None:
+        fig, axes = plt.subplots(figsize=(5, 5))
+    tp = triplot(mesh, axes=axes, **kwargs)
+    axes.axis(False)
+    return fig, axes, tp
+
+
+def standardize(arr):
+    """
+    Apply standardization based on mean and standard deviation
+    """
+    normed = (arr - np.mean(arr)) / np.std(arr)
+    normed -= np.min(normed)
+    normed += 1
+    return normed
+
+
+def lp_normalize(arr, p=2):
+    """
+    Normalize in L^p space.
+    """
+    retval = arr**p
+    denom = sum(retval) ** (1 / p)
+    retval /= denom
+    return retval
+
+
+def reorder_indicators(indicators):
+    """
+    Temporary function that changes the shape of the indicator list for plotting purposes.
+    """
+    temp = [k[0] for k in indicators]
+    temp_indicators = [temp]
+    return temp_indicators
+
+
+def standardize_indicators(indicators, std_fn=standardize, **kwargs):
+    """
+    Apply given form of standardization or normalization to the monitor function.
+    """
+    P0 = indicators[0][0].function_space()
+    retval = []
+    for i in range(len(indicators)):
+        t1 = []
+        for j in range(len(indicators[0])):
+            t1.append(Function(P0, val=std_fn(indicators[i][j].dat.data, **kwargs)))
+        retval.append(t1)
+    return retval
+
+
+def get_get_monitor(mesh, i, indicators):
+    """
+    Get the given monitor function. This returns something compatible with the mover object.
+    """
+
+    def get_monitor(mesh):
+        P0 = FunctionSpace(mesh, "DG", 0)
+        f = Function(P0)
+        f.project(indicators[0][i])
+        return f
+
+    return get_monitor
+
+
 def main():
-    print("Program starting")
     n = 32
     fields = ["u"]
-    meshes = [
-        UnitSquareMesh(n, n, diagonal="left"),
-        UnitSquareMesh(n, n, diagonal="left"),
-    ]
+    mesh = UnitSquareMesh(n, n, diagonal="left")
+    # We use a pyroteus.MeshSeq even though we have only one mesh because it allows us to use MeshSeq.indicate_errors()
+    meshes = [mesh]
     end_time = 0.5
     dt = 1 / n
     num_subintervals = len(meshes)
@@ -136,111 +207,80 @@ def main():
         get_qoi=get_qoi,
         qoi_type="end_time",
     )
+    print("Solving the problem on a single, uniform mesh")
+    # Get the forward and adjoint solutions and DWR error indicators
     solutions, indicators = mesh_seq.indicate_errors(
         enrichment_kwargs={"enrichment_method": "p"}
     )
 
-    vmax, vmin = -np.inf, np.inf
-    for i, mesh_i in enumerate(indicators):
-        for j, e_t in enumerate(indicators[i]):
-            vmax = np.max((vmax, np.max(np.log(np.abs(e_t.dat.data)))))
-            vmin = np.min((vmin, np.min(np.log(np.abs(e_t.dat.data)))))
-
-    figsize = (
-        2 * len(solutions["u"]["forward"][0]),
-        2 * len(solutions["u"]["forward"][0]),
+    print("Pre-processing for movement")
+    standardized_indicators = standardize_indicators(indicators)
+    # Create new meshes to adapt. We  use as many meshes as we have exports of the monitor function
+    meshes = [UnitSquareMesh(n, n, diagonal="left") for i in range(len(indicators[0]))]
+    num_subintervals = len(meshes)
+    time_partition = TimePartition(
+        end_time, num_subintervals, dt, fields, timesteps_per_export=1
     )
-    # print(vmax, vmin)
-    # vmax = -12
-    # vmin = -70
-    # fig, axs, tcs = plot_indicator_snapshots2(
-    #     indicators,
-    #     time_partition,
-    #     levels=np.linspace(vmin, vmax, 100),
-    #     vmax=vmax,
-    #     vmin=vmin,
-    #     cmap="viridis",
-    #     figsize=figsize,
-    # )
-    # cbar = fig.colorbar(tcs[1][-3], ax=axs, location="top")
-    # print(np.exp(-24.6), np.exp(-14))
-    # fig.savefig("fig1_dwr.jpg")
-    # smoothing of monitor function:
-    gamma = 2  # Refinement level
-    eta_b = 5e-8
-    funcs = []
-    P0 = indicators[i][0].function_space()
-    for i, mesh_i in enumerate(indicators):
-        #     area = areas[i]
-
-        max_errs = np.zeros(indicators[i][0].dat.data.shape)
-        fns = []
-        for j, e_t in enumerate(indicators[i]):
-            val = np.array(e_t.dat.data)
-            fn = Function(P0, val=val)
-            fns.append(fn)
-        funcs.append(fns)
-    # print(len(funcs))
-    # print(len(funcs[1]))
-    # fig, axs, tcs = plot_indicator_snapshots(
-    #     funcs, time_partition, cmap="viridis", figsize=figsize
-    # )
-    # cbar = fig.colorbar(tcs[-1][-1], ax=axs, location='top')
-    # plt.suptitle("Monitor function")
-    # for i in range(axs.shape[1]):
-    #     for j in range(axs.shape[0]):
-    #         fig.colorbar(tcs[i][j], ax=axs[j, i])
-    # plt.suptitle("Unsmoothed Monitor Function")
-    # fig.savefig("fig2_unsmooth.jpg")
-
-    P = mesh_seq.time_partition
-    N = 40  # Constant from paper, but see the later comment
-    for num_smooth in range(1, 16):
-        smoothed_fns = []
-        # num_smooth = 16
-        print("Starting smoothing #{}".format(num_smooth))
-        print(type(CellSize(mesh_seq[i])))
-        for i in range(len(funcs)):
-            fns = []
-            function_space = funcs[i][0].function_space()
-            dt = P.timesteps[i]
-            delX = Constant(1 / (2 * n))
-            K = Constant(N * delX**2 / dt)
-            t_start, t_end = P.subintervals[i]
-            t = t_start
-            f_bound = Function(function_space)
-            f_smooth = Function(function_space)
-            v = TestFunction(f_bound.function_space())
-            F = (
-                inner(f_smooth - f_bound / dt, v)
-                - K * (inner(dot(nabla_grad(f_smooth), nabla_grad(f_smooth)), v))
-            ) * dx
-            for j in range(len(funcs[i])):
-                # this should be the same as while t < t_end - 1e-5:
-                f_bound.assign(funcs[i][j])
-                for k in range(num_smooth):
-                    # Potentially have a k-loop here and smooth N times?
-                    # print(k)
-                    solve(F == 0, f_smooth)
-                    f_bound.assign(f_smooth)
-                t += dt
-                fns.append(Function(function_space, val=f_smooth.dat.data))
-            smoothed_fns.append(fns)
-
-        # print(len(smoothed_fns), len(smoothed_fns[0]), type(smoothed_fns[0][0]))
-        print(
-            np.min(smoothed_fns[-1][-1].dat.data), np.max(smoothed_fns[-1][-1].dat.data)
+    rtol = 1e-2
+    maxiter = 100
+    method = "quasi_newton"
+    for i, mesh in enumerate(meshes):
+        print("Starting on mesh {}".format(i))
+        get_mon = get_get_monitor(mesh, i, standardized_indicators)
+        mover = MongeAmpereMover(
+            mesh, get_mon, method=method, maxiter=maxiter, rtol=rtol
         )
-
-        fig, axs, tcs = plot_indicator_snapshots(
-            smoothed_fns, time_partition, cmap="viridis", figsize=figsize
+        mover.move()
+        # if i < len(meshes) - 1:
+        #     meshes[i + 1].coordinates.assign(mesh.coordinates)
+    print("Computing quality metrics")
+    quality_measures = {}
+    for i, mesh in enumerate(meshes):
+        quality_measures[i] = {}
+        quality_measures[i]["min_angle"] = get_min_angles2d(mesh)
+        quality_measures[i]["aspect_ratio"] = get_aspect_ratios2d(mesh)
+        quality_measures[i]["skewness"] = get_skewnesses2d(mesh)
+        quality_measures[i]["scaled_jac"] = get_scaled_jacobians2d(mesh)
+        print("Mesh {}".format(i))
+        print("Metric\t\tMin\t\tMax")
+        for key in quality_measures[i].keys():
+            print(
+                "{}\t\t{:.3f}\t\t{:.3f}".format(
+                    key,
+                    np.min(quality_measures[i][key].dat.data),
+                    np.max(quality_measures[i][key].dat.data),
+                )
+            )
+    figsize = (10, 20)
+    fig, axs = plt.subplots(4, 2, figsize=figsize)
+    tps = []
+    tp = []
+    for i in range(len(meshes)):
+        row, col = i // (len(meshes) // 4), i % 2
+        _, _, plot = plot_mesh(meshes[i], fig=fig, axes=axs[row, col])
+        tp.append(plot)
+        min_ar, max_ar = np.min(quality_measures[i]["aspect_ratio"].dat.data), np.max(
+            quality_measures[i]["aspect_ratio"].dat.data
         )
-        for i in range(axs.shape[1]):
-            for j in range(axs.shape[0]):
-                fig.colorbar(tcs[i][j], ax=axs[j, i])
-        plt.suptitle("Smoothed Monitor Function ({})".format(num_smooth))
-        fig.savefig("fig3_smooth_{}.jpg".format(num_smooth))
-        print("Wrote to: {}".format("fig3_smooth_{}.jpg".format(num_smooth)))
+        if time_partition is not None:
+            time = 2 * i * dt
+            axs[row, col].set_title(
+                "t={:.3f}, Aspect Ration Min/Max:{:.3f}{:.3f}".format(
+                    time, min_ar, max_ar
+                )
+            )
+    tps.append(tp)
+    fig.tight_layout(pad=0.5)
+    plt.suptitle("Adapted meshes")
+    adapt_filename = "adapted_meshes"
+    fig.savefig(adapt_filename)
+    print("Saved adapted mesh figure to: {}".format(adapt_filename))
+
+    for key in quality_measures[0].keys():
+        out_file = File(f"quality_measures/{key}.pvd")
+        for i in range(len(meshes)):
+            out_file.write(quality_measures[i][key])
+        print("Wrote to {}".format(out_file))
 
 
 if __name__ == "__main__":
